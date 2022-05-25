310c4501f0ba377cdb695bdb830b39bdd2b88d46
CK Tan <cktanx@gmail.com>
2022-05-24 17:31:18 -0700

    update xplan; add tests/q1.plan and q6.plan

--- q6.sql
+++
@@ -1,1 +0,0 @@
-SELECT SUM(l_extendedprice * l_discount) FROM "lineitem*" WHERE l_shipdate >= '1994-01-01' AND l_shipdate < '1995-01-01' AND l_discount >= 0.050000 AND l_discount <= 0.070000 AND l_quantity < 24.000000
